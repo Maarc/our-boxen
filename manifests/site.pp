5292855d39041969f4a05e6e82a7fd15f6bd9f2f
Maarc <marc@zottner.com>
2015-01-25 00:00:28 +0100

    my-boxen updated with most of the applications I am using

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,9 +53,10 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
-  #include dnsmasq
   include git
   include hub
+
+  #include dnsmasq
   #include nginx
 
   # fail if FDE is not enabled
